992ecb70bb24444d5ef156fff99da0b5f54206ab
Diego Caceres <diego.rca.23@gmail.com>
2021-03-25 14:16:18 -0300

    Second Commit - Create fisrt app: POST

---
+++ posts/apps.py
@@ -0,0 +1,9 @@
+"""Post aplication module"""
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Post aplication settings"""
+    name = 'posts'
+    varbose_name = 'Posts'
