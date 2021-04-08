0f8d8ff040dd2ea1cccf2b1c2ddf3a6785b645de
Diego Caceres <diego.rca.23@gmail.com>
2021-04-08 17:54:28 -0300

    Models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
+"""Post models: ejemplo"""
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """User model."""
+
+    email = models.EmailField()
+    password = models.CharField()
+    
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+    bio = models.TextField()
+
+    birthdate = models.DateField()
+
+    created = models.DateTimeField()
+    modified = models.DateTimeField()
